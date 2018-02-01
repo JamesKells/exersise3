7833c6f6acb214987d6b6b23f49faa1080a5fa2d
James Kells <james.kells@rocketmail.com>
2018-02-01 14:27:46 -0500

    Initial commit

---
+++ src/com/kells/exercise3/Main.java
@@ -0,0 +1,31 @@
+package com.kells.exercise3;
+import java.util.Scanner;
+public class Main {
+
+    static double temp = 32;
+
+    public static void main(String[] args) {
+
+        while(temp >= -460){
+            input();
+            celsius();
+            output();
+        }
+    }
+
+    static void input() {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter your temp: ");
+        temp = reader.nextDouble();
+    }
+
+    static void celsius() {
+        temp = (temp - 32) * 5 / 9;
+    }
+
+    static void output(){
+        System.out.println(temp);
+    }
+
+
+}
